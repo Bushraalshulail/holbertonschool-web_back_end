9a70e9392a639fed55e15672ddb2305ab31a0520
Bushraalshulail <109034337+Bushraalshulail@users.noreply.github.com>
2025-09-07 16:38:45 +0300

    Update utils.js

--- Node_JS_basic/full_server/utils.js
+++ Node_JS_basic/full_server/utils.js
@@ -1,19 +1,34 @@
-import fs from 'fs';
+const fs = require('fs');
 
-export function readDatabase(filePath) {
+function readDatabase(path) {
   return new Promise((resolve, reject) => {
-    fs.readFile(filePath, 'utf8', (err, data) => {
-      if (err) reject(err);
-      else {
-        const lines = data.trim().split('\n');
-        const studentsByField = {};
-        lines.slice(1).forEach((line) => {
-          const [firstname, , , field] = line.split(',');
-          if (!studentsByField[field]) studentsByField[field] = [];
-          studentsByField[field].push(firstname);
-        });
-        resolve(studentsByField);
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(Error(err));
+        return;
       }
+      const content = data.toString().split('\n');
+
+      let students = content.filter((item) => item);
+
+      students = students.map((item) => item.split(','));
+
+      const fields = {};
+      for (const i in students) {
+        if (i !== 0) {
+          if (!fields[students[i][3]]) fields[students[i][3]] = [];
+
+          fields[students[i][3]].push(students[i][0]);
+        }
+      }
+
+      delete fields.field;
+
+      resolve(fields);
+
+      //   return fields;
     });
   });
 }
+
+export default readDatabase;
